e7d2b600994051fd668207be5f724f1d5a5d9355
Jack Stoetzel <stoetzeljt@gmail.com>
2019-11-30 11:24:19 -0500

    Removes overflow

--- Project2/main.c
+++ Project2/main.c
@@ -19,6 +19,7 @@ int main(){
     Articles arxiv[1000];
     int meta;
     int i, k;
+
     meta = open("arxiv-metadata.txt", O_RDONLY);
     if(meta < 0)
 	{
@@ -29,26 +30,51 @@ int main(){
     char buf;
     read(meta, &buf, 1);
     for(k = 0; meta; k++){
+        // Parsing the article's ID
         read(meta, &buf, 1);
         for(i = 0; buf != '\n' && i < IDSIZE; i++){
             arxiv[k].ID[i] = buf;
             read(meta, &buf, 1);
         }
+        if(i == IDSIZE){
+            for(i = 0; buf != '\n'; i++){
+                read(meta, &buf, 1);
+            }
+        }
+        // Parsing the article's title
         read(meta, &buf, 1);
         for(i = 0; buf != '\n' && i < TTSIZE; i++){
             arxiv[k].title[i] = buf;
             read(meta, &buf, 1);
         }
+        if(i == TTSIZE){
+            for(i = 0; buf != '\n'; i++){
+                read(meta, &buf, 1);
+            }
+        }
+        // Parsing the article's author
         read(meta, &buf, 1);
         for(i = 0; buf != '\n' && i < AUSIZE; i++){
             arxiv[k].authors[i] = buf;
             read(meta, &buf, 1);
         }
+        if(i == AUSIZE){
+            for(i = 0; buf != '\n'; i++){
+                read(meta, &buf, 1);
+            }
+        }
+        // Parsing the article's abstract
         read(meta, &buf, 1);
         for(i = 0; buf != '\n' && i < ABSIZE; i++){
             arxiv[k].abstract[i] = buf;
             read(meta, &buf, 1);
         }
+        if(i == ABSIZE){
+            for(i = 0; buf != '\n'; i++){
+                read(meta, &buf, 1);
+            }
+        }
+        // Removing the +++++
         read(meta, &buf, 1);
         for(i = 0; buf != '\n'; i++){
             read(meta, &buf, 1);
@@ -57,9 +83,10 @@ int main(){
         printf("%s \n", arxiv[k].title);
         printf("%s \n", arxiv[k].authors);
         printf("%s \n", arxiv[k].abstract);
+        puts("");
         sleep(3);
 }
     //printf("%c \n", buf);
 
 
-}
+} 
