2230a64fb4d0f1489d281e22c6cd6b736997d8a7
Jack Stoetzel <stoetzeljt@gmail.com>
2019-12-01 17:41:57 -0500

    Made changes

--- Project2/main.c
+++ Project2/main.c
@@ -10,19 +10,13 @@ const int TTSIZE = 256;
 const int AUSIZE = 256;
 const int ABSIZE = 4096;
 
-typedef struct ARTICLES{
-    char ID [32];
-    char title[256];
-    char authors[256];
-    char abstract[4096];
-
-}Articles;
-
 int main(){
-    Articles arxiv[1000];
+    // 1,614,712
+    Articles arxiv;
+
     char word[32];
     int meta;
-    int i, k;
+    long long i, k;
 
     meta = open("arxiv-metadata.txt", O_RDONLY);
     if(meta < 0)
@@ -33,11 +27,11 @@ int main(){
 
     char buf;
     read(meta, &buf, 1);
-    for(k = 0; meta; k++){
+    for(k = 0; k < 1614712; k++){
         // Parsing the article's ID
         read(meta, &buf, 1);
         for(i = 0; buf != '\n' && i < IDSIZE; i++){
-            arxiv[k].ID[i] = buf;
+            arxiv.ID[i] = buf;
             read(meta, &buf, 1);
         }
         if(i == IDSIZE){
@@ -45,10 +39,10 @@ int main(){
                 read(meta, &buf, 1);
             }
         }
-            // Parsing the article's title
+        // Parsing the article's title
         read(meta, &buf, 1);
         for(i = 0; buf != '\n' && i < TTSIZE; i++){
-            arxiv[k].title[i] = buf;
+            arxiv.title[i] = buf;
             read(meta, &buf, 1);
         }
         if(i == TTSIZE){
@@ -56,10 +50,10 @@ int main(){
                 read(meta, &buf, 1);
             }
         }
-            // Parsing the article's author
+        // Parsing the article's author
         read(meta, &buf, 1);
         for(i = 0; buf != '\n' && i < AUSIZE; i++){
-            arxiv[k].authors[i] = buf;
+            arxiv.authors[i] = buf;
             read(meta, &buf, 1);
         }
         if(i == AUSIZE){
@@ -67,43 +61,54 @@ int main(){
                 read(meta, &buf, 1);
             }
         }
-            // Parsing the article's abstract
-        int wordPos = 0;
+        // Parsing the article's abstract
+        int wordPos = -2;
         read(meta, &buf, 1);
         for(i = 0; buf != '\n' && i < ABSIZE; i++){
                 //printf("%d %c \n", i, buf);
-            if(buf != ' ' && i > 2){
-                    //printf("HERE I\n");
+            if(buf != ' ' && i > 1){
+                //printf("HERE %c \n", buf);
                 word[wordPos + i] = buf;
             }
-
-            else if(buf == ' ' && i > 2){
-                    //printf("HERE E\n");
-                printf("%s ", word);
+            else if(buf == ' ' && i > 1){
+                //printf("%s ", word);        // printf("%s \n", arxiv.ID);
+        // printf("%s \n", arxiv.title);
+        // printf("%s \n", arxiv.authors);
+        // printf("%s \n", arxiv.abstract);
+        // puts("");
                     // Add word to tree w/ article ID
                 wordPos = (i + 1) * (-1);
                 memset(word, 0, sizeof(word));
             }
 
-            arxiv[k].abstract[i] = buf;
+            arxiv.abstract[i] = buf;
             read(meta, &buf, 1);
+
+            //sleep(1);
         }
         if(i == ABSIZE){
             for(i = 0; buf != '\n'; i++){
                 read(meta, &buf, 1);
             }
         }
-            // Removing the +++++
+        // Removing the +++++
         read(meta, &buf, 1);
         for(i = 0; buf != '\n'; i++){
             read(meta, &buf, 1);
         }
-        printf("%s \n", arxiv[k].ID);
-        printf("%s \n", arxiv[k].title);
-        printf("%s \n", arxiv[k].authors);
-            //printf("%s \n", arxiv[k].abstract);
+
+        printf("%s \n", arxiv.ID);
+        printf("%s \n", arxiv.title);
+        printf("%s \n", arxiv.authors);
+        printf("%s \n", arxiv.abstract);
         puts("");
-        sleep(3);
+
+        // Add arxiv to the article tree
+
+        memset(arxiv.ID, 0, sizeof(arxiv.ID));
+        memset(arxiv.title, 0, sizeof(arxiv.title));
+        memset(arxiv.authors, 0, sizeof(arxiv.authors));
+        memset(arxiv.abstract, 0, sizeof(arxiv.abstract));
     }
     //printf("%c \n", buf);
-} 
+}
