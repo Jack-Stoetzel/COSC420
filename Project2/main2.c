c6e6b1c5e7549e9dd5acad645dc07cc7bcbd0531
Jack Stoetzel <stoetzeljt@gmail.com>
2019-12-05 13:02:48 -0500

    Cleaned

--- Project2/main2.c
+++ Project2/main2.c
@@ -1,14 +1,20 @@
-//  TODO
-//    Figure out how to add word to the word tree starting with the ID.
-//      Start with (buf == ' ' || buf == '\n')??? or  (buf == ' ') at the beginning
-//      
-//    TEST THIS RB tree
-//    Test inserting all articles into the list and searching an article ID
-//    If the first RB tree works, make another for words with a LL for IDs with that word
-//
-//  EXTA
-//  Compute the adjacentcy matrix as we insert IDs into the the word list
+/*
+    arxiv-metadata.txt
+    Lines:          8,073,560
+    Articles:       1,614,712
+    Root Article:   1701.01948
 
+    TODO
+        Figure out how to add word to the word tree starting with the ID.
+            Start with (buf == ' ' || buf == '\n')??? or  (buf == ' ') at the beginning
+
+        TEST THIS RB tree
+        Test inserting all articles into the list and searching an article ID
+        If the first RB tree works, make another for words with a LL for IDs with that word
+
+    EXTA
+        Compute the adjacentcy matrix as we insert IDs into the the word list
+*/
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -22,7 +28,6 @@ int main(){
     Article arxiv;
 
     ArticleNode *root = NULL;
-    //char word[400];
     int meta;
     long long i, k;
     char info[37000];
@@ -36,26 +41,34 @@ int main(){
 
     char buf;
     int pos = 0;    // Will determine struct element 0. ID 1. Title 2. Author 3. Abstract
-    int size[5];   
+    int size[5];
+    for(i = 0; i < 5; i++){
+        size[i] = 0;
+    }
     while(read(meta, &buf, 1) > 0){
         info[size[pos]] = buf;
         size[pos]++;
         if((buf == ' ' || buf == '\n') && pos == 3){
             // Insert word to word tree w/ ID
+            memset(info, '\0', sizeof(info));
             if(buf == '\n'){
-                pos++;  
+                pos++;
             }
+            size[pos] = 0;
         }
         else if(buf == '\n'){
             if(pos == 0){
+                // ID
                 arxiv.ID = (char*) calloc(size[0], sizeof(char));
                 strcpy(arxiv.ID, info);
             }
             else if(pos == 1){
+                // Title
                 arxiv.title = (char*) calloc(size[1], sizeof(char));
                 strcpy(arxiv.title, info);
             }
             else if(pos == 2){
+                // Author
                 arxiv.authors = (char*) calloc(size[2], sizeof(char));
                 strcpy(arxiv.authors, info);
             }
@@ -64,12 +77,12 @@ int main(){
                 printf("%s \n", arxiv.ID);
                 printf("%s \n", arxiv.title);
                 printf("%s \n\n", arxiv.authors);
-               // sleep(2);
+                //sleep(2);
                 memset(arxiv.ID, '\0', sizeof(size[0]));
                 memset(arxiv.title, '\0', sizeof(size[1]));
                 memset(arxiv.authors, '\0', sizeof(size[2]));
-
-                for(int i = 0; i < 4; i++){
+                int i;
+                for(i = 0; i < 5; i++){
                     size[i] = 0;
                 }
                 pos = -1;
@@ -78,89 +91,6 @@ int main(){
             memset(info, '\0', sizeof(info));
         }
     }
-
-/*
-        // Parsing the article's ID
-        read(meta, &buf, 1);
-        for(i = 0; buf != '\n' && i < IDSIZE; i++){
-            //printf("%c\n", buf);
-            arxiv.ID[i] = buf;
-            read(meta, &buf, 1);
-        }
-        arxiv.ID[i] = '\0';
-        if(i == IDSIZE){
-            for(i = 0; buf != '\n'; i++){
-                read(meta, &buf, 1);
-            }
-        }
-        // Parsing the article's title
-        read(meta, &buf, 1);
-        for(i = 0; buf != '\n' && i < TTSIZE; i++){
-            arxiv.title[i] = buf;
-            read(meta, &buf, 1);
-        }
-        arxiv.title[i] = '\0';
-        if(i == TTSIZE){
-            for(i = 0; buf != '\n'; i++){
-                read(meta, &buf, 1);
-            }
-        }
-        // Parsing the article's author
-        read(meta, &buf, 1);
-        for(i = 0; buf != '\n' && i < AUSIZE; i++){
-            arxiv.authors[i] = buf;
-            read(meta, &buf, 1);
-        }
-        arxiv.authors[i] = '\0';
-        if(i == AUSIZE){
-            for(i = 0; buf != '\n'; i++){
-                read(meta, &buf, 1);
-            }
-        }
-        // Parsing the article's abstract
-        int wordPos = -2;
-        read(meta, &buf, 1);
-        for(i = 0; buf != '\n' && i < ABSIZE; i++){
-                //printf("%d %c \n", i, buf);
-            if(buf != ' ' && i > 1){
-                //printf("HERE %c \n", buf);
-                word[wordPos + i] = buf;
-            }
-            else if(buf == ' ' && i > 1){
-                    // Add word to tree w/ article ID
-                wordPos = (i + 1) * (-1);
-                memset(word, 0, sizeof(word));
-            }
-
-            arxiv.abstract[i] = buf;
-            read(meta, &buf, 1);
-        }
-        arxiv.abstract[i] = '\0';
-        if(i == ABSIZE){
-            for(i = 0; buf != '\n'; i++){
-                read(meta, &buf, 1);
-            }
-        }
-        // Removing the +++++
-        read(meta, &buf, 1);
-        for(i = 0; buf != '\n'; i++){
-            read(meta, &buf, 1);
-        }
-
-        //printf("%s \n", arxiv.ID);
-        // printf("%s \n", arxiv.title);
-        // printf("%s \n", arxiv.authors);
-        // printf("%s \n", arxiv.abstract);
-        // puts("");
-
-        // Add arxiv to the article tree
-        insert(root, &arxiv);
-        printf("main root = %s\n", root -> article -> ID);
-        memset(arxiv.ID, '\0', sizeof(arxiv.ID));
-        memset(arxiv.title, '\0', sizeof(arxiv.title));
-        memset(arxiv.authors, '\0', sizeof(arxiv.authors));
-        memset(arxiv.abstract, '\0', sizeof(arxiv.abstract));
-    */
     inorder(root);
     //printf("%c \n", buf);
 }
