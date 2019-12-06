f19618af9f33e5db39a80ea98f2f1f2bfa07c657
Jack Stoetzel <stoetzeljt@gmail.com>
2019-12-06 10:38:00 -0500

    Added comments

--- Project2/wtree.h
+++ Project2/wtree.h
@@ -1,3 +1,7 @@
+/* 	Tree of words
+	The goal is to have a BST that is indexed by words from arxiv article abstracts.
+	Each node will contain a word, and a linked list of article IDs whos abstracts contain that word. 
+*/
 #ifndef WTREE_H
 #define WTREE_H
 
